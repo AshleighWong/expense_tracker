1b9691fa74db73553771eed136ccee6ba7c927db
AshleighWong <ashleightwong@gmail.com>
2024-07-31 03:15:19 -0400

    purples

--- app/page.js
+++ app/page.js
@@ -61,15 +61,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className="text-4xl p-4 text-center">Expense Tracker</h1>
-        <div className="bg-slate-800 p-4 rounded-lg">
+        <div className="bg-slate-900 p-4 rounded-lg">
           <form className="grid grid-cols-6 items-center text-black">
-            <input value={newItem.name} onChange={(e)=> setNewItem({...newItem, name: e.target.value})} className="col-span-3 p-3 border-2" type="text" placeholder="Enter Item"></input>
-            <input value={newItem.price} onChange={(e)=> setNewItem({...newItem, price: e.target.value})} className="col-span-2 p-3 border-2 mx-3" type="text" placeholder="Enter $"></input>
-            <button onClick={addItem} className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl" type="submit">+</button>
+            <input value={newItem.name} onChange={(e)=> setNewItem({...newItem, name: e.target.value})} className="col-span-3 p-3 border-2 border-zinc-400" type="text" placeholder="Enter Item"></input>
+            <input value={newItem.price} onChange={(e)=> setNewItem({...newItem, price: e.target.value})} className="col-span-2 p-3 border-2 mx-3 border-" type="text" placeholder="Enter $"></input>
+            <button onClick={addItem} className="text-white bg-indigo-900 hover:bg-indigo-700 p-3 text-xl" type="submit">+</button>
           </form>
           <ul>
             {items.map((item, id)=> (
-              <li key={id} className="my-4 w-full flex justify-between bg-slate-950">
+              <li key={id} className="my-4 w-full flex justify-between bg-indigo-900">
                   <div className="p-4 w-full flex justify-between">
                     <span className="mx-2 capitalize">{item.name}</span>
                     <span>{item.price}</span>
